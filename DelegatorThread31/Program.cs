da94ed28ec424259078a049ab5131969e25aa35f
Gihun Ham <ham@e-mirim.hs.kr>
2023-08-31 15:27:10 +0900

    초기 커밋.

---
+++ DelegatorThread31/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DelegatorThread31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread threadA = new Thread(DoSomething);
+            Thread threadB = new Thread(delegate ()
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    Console.Write("B");
+                }
+            });
+            Thread threadC = new Thread(() => {
+                for (int i = 0; i < 1000; i++)
+                {
+                    Console.Write("C");
+                }
+            });
+        }
+
+        public static void DoSomething() { 
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write("A");
+            }
+        }
+    }
+}
